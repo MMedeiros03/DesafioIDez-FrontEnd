48343fbd7a7af60868aeeb0d828006034e8f2798
Matheus Medeiros Oselame <matheusmedeiros2003@gmail.com>
2026-01-13 10:13:14 -0300

    fix: atualizado nome da pasta no import da homePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomePage from './pages/Home'
+import HomePage from './Pages/Home'
 
 createRoot(document.getElementById('root')).render(
   <HomePage />
